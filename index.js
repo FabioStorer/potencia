2a04eee847ad5d7eb023109d095aaef3f3508688
Fabio.Storer <fabiostorer19@gmail.com>
2024-06-27 17:08:46 -0300

    feat: adicionado a logica

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const prompt = require('prompt-sync')();
-let resultado = 0;
 
-let base = prompt('Digite um número que irei calcular a potencia do mesmo:');
-let expoente = prompt('Digite agora o expoente:');
-let aux = 0;
-base = Number(base)
-expoente = Number(expoente)
+let base = +prompt('Digite um número que irei calcular a potencia do mesmo:');
+let expoente = +prompt('Digite agora o expoente:');
+let aux = base;
 aux = Number(aux)
+let resultado = base
 
-for (let i = 0; i <= expoente; i++) {
-    resultado = (resultado + (base + base))
-    console.log(resultado)
+for (let i = 1; i < expoente; i++) {
+    for (let j = 1; j < base; j++) {
+        resultado += aux
+    }
+    aux = resultado
 }
-resultado = resultado + base
 
 console.log(`A potencia do número informado é ${resultado}.`);
