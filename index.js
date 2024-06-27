25b4a8672ac8bc978df31bb842ec063e9a0ebd3e
Fabio.Storer <fabiostorer19@gmail.com>
2024-06-27 16:08:49 -0300

    :first commit: codigo funcional, sem utilização de branch

---
+++ index.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')();
+let resultado = 0;
+
+let base = prompt('Digite um número que irei calcular a potencia do mesmo:');
+let expoente = prompt('Digite agora o expoente:');
+
+for (let i = 1; i <= expoente; i++) {
+    resultado = resultado + (base * expoente)
+    console.log(resultado)
+}
+
+console.log(`A potencia do número informado é ${resultado}.`);
