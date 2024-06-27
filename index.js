9ffc2d40691858e03423bc3bdc95ec28a11c106e
Fabio.Storer <fabiostorer19@gmail.com>
2024-06-27 17:17:25 -0300

    feat: adicionado while para repetir o codigo enquanto a condição for verdadeira

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const prompt = require('prompt-sync')();
 
+while (true) {
 let base = +prompt('Digite um número que irei calcular a potencia do mesmo:');
 let expoente = +prompt('Digite agora o expoente:');
 let aux = base;
 aux = Number(aux)
 let resultado = base
 
-for (let i = 1; i < expoente; i++) {
-    for (let j = 1; j < base; j++) {
-        resultado += aux
-    }
-    aux = resultado
-}
 
-console.log(`A potencia do número informado é ${resultado}.`);
+    for (let i = 1; i < expoente; i++) {
+        for (let j = 1; j < base; j++) {
+            resultado += aux
+        }
+        aux = resultado
+    }
+    console.log(`A potencia do número informado é ${resultado}.`);
+}
